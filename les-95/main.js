531ccce2850fe0b43efad84bb44ff49cf2099f29
denis0606 <135761781+denis0606@users.noreply.github.com>
2025-12-10 21:24:08 +0300

    Add files via upload

---
+++ les-95/main.js
@@ -0,0 +1,8 @@
+const firstName = 'Denis';
+const lastName = 'Zimin';
+let age = 29;
+let favoriteColor = 'green';
+let unfavouriteColor = 'red';
+
+console.log(firstName, lastName, age, 'y.o.', favoriteColor, unfavouriteColor);
+
