c04730d0ae77e1a0eee94bf7f742062639eb1165
denis0606 <135761781+denis0606@users.noreply.github.com>
2025-12-10 21:23:03 +0300

    Delete les-95 directory

--- les-95/main.js
+++
@@ -1,8 +0,0 @@
-const firstName = 'Denis';
-const lastName = 'Zimin';
-let age = 29;
-let favoriteColor = 'green';
-let unfavouriteColor = 'red';
-
-console.log(firstName, lastName, age, 'y.o.', favoriteColor, unfavouriteColor);
-
